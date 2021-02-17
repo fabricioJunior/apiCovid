66beeec7bdd07125bb1f53ef32b685f619512549
fabricioJunior <34898688+fabricioJunior@users.noreply.github.com>
2021-02-17 16:41:18 -0300

    API

---
+++ covidAPI/covidAPI/covidAPI/API Externa/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace covidAPI.API_Externa
+{
+    public static class Utils
+    {
+        public static async Task<string> GetAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new System.Uri("http://localhost:53557/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
